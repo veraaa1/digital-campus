bd82793e7d4d95ba0735543d37ac9d633d74d08f
veraaa1 <1301778436@qq.com>
2019-04-05 19:21:08 +0800

    更改绩效自评的样式

--- src/views/KPI.vue
+++ src/views/KPI.vue
@@ -1,24 +1,29 @@
 <template>
   <div class="kpi">
     <h1>绩效自评</h1>
-    <form action="">
-        <label for="last">上月教学任务</label>
-        <textarea name="" id="last" cols="30" rows="10"></textarea>
-        <label for="real">实际完成教学任务</label>
-        <textarea name="" id="real" cols="30" rows="10"></textarea>
-        <label for="last">上月教学任务</label>
-        <label for="finish-rate">任务达成率</label>
-        <input type="text" id="finish-rate">
-        <label for="self">上月教学任务自评</label>
-        <textarea name="" id="self" cols="30" rows="10"></textarea>
-        <label for="now">本月教学任务</label>
-        <textarea name="" id="now" cols="30" rows="10"></textarea>
-        <label for="plan">本月教学计划</label>
-        <textarea name="" id="plan" cols="30" rows="10"></textarea>
-        <span>审批人</span>
-        <router-link to="/">添加审批人</router-link>
-        <button type="submit">提交</button>
-    </form>
+    <el-form>
+      <span>审批人</span>
+      <router-link to="/">添加审批人</router-link>
+      <el-form-item label="上月教学任务">
+        <el-input type="textarea"></el-input>
+      </el-form-item>
+      <el-form-item label="实际完成教学任务">
+        <el-input type="textarea"></el-input>
+      </el-form-item>
+      <el-form-item label="任务达成率">
+        <el-input type="text"></el-input>
+      </el-form-item>
+      <el-form-item label="上月教学任务自评">
+        <el-input type="textarea"></el-input>
+      </el-form-item>
+      <el-form-item label="本月教学任务">
+        <el-input type="textarea"></el-input>
+      </el-form-item> 
+      <el-form-item label="本月教学计划">
+        <el-input type="textarea"></el-input>
+      </el-form-item>  
+      <el-button type="primary">提交</el-button>
+    </el-form>
   </div>
 </template>
 <script>
